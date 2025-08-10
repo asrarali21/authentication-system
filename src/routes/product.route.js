3966a76d0313278ec35bac4159301ef6f5885f78
asrarali21 <mohdasrar3825@gmail.com>
2025-08-10 14:43:26 +0530

    add admin login , verify admin middleware

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middleware/multer.middleware.js";
 import { addProduct, listProduct, removiingProduct, singleproduct } from "../controller/product.controller.js";
+import verifyadmin from "../middleware/verifyadmin.middleware.js";
 
 
 
@@ -8,9 +9,9 @@ const Productroute = Router()
 
 
 
-Productroute.route("/addproduct").post(upload.array("image" , 5), addProduct)
+Productroute.route("/addproduct").post(verifyadmin,upload.array("image" , 5), addProduct)
 Productroute.route("/productlist").get(listProduct)
-Productroute.route("/deleteproduct/:id").delete(removiingProduct)
+Productroute.route("/deleteproduct/:id").delete(verifyadmin,removiingProduct)
 Productroute.route("/singleproduct/:id").get(singleproduct)
 
 
