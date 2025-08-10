0d30184db165fb62c2af28b9ef5325c971bd229f
asrarali21 <mohdasrar3825@gmail.com>
2025-08-10 19:01:11 +0530

    import fix  , added frontend , login form

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+
+   <>
+   <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
+   </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1 className='bg-amber-300'>click me</h1>
+    </div>
+  )
+}
+
+export default Home
