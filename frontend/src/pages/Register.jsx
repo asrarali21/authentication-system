487a99ef16b707607af18ed11654729a629845cb
asrarali21 <mohdasrar3825@gmail.com>
2025-08-11 21:07:44 +0530

    minor fixes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -21,9 +21,12 @@ console.log(formData);
 
   async function handleformsubmit(e) {
       e.preventDefault();
-      await  axios.post("http://localhost:8000/api/v1/users/registerUser" , formData ,{
+    const response =  await  axios.post("http://localhost:8000/api/v1/users/registerUser" , formData ,{
   withCredentials: true
 })
+console.log(response);
+    setFormData({ name: "", email: "", password: "" });
+
    }
 
   return (
@@ -39,7 +42,7 @@ console.log(formData);
               <form class="space-y-4 md:space-y-6"  onSubmit={handleformsubmit}>
                     <div>
                       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">name</label>
-                      <input type="text" name="name" id="name" placeholder="enter you name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} required/>
+                      <input type="text" name="name" id="name" placeholder="enter you name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})}required/>
                   </div>
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
@@ -51,11 +54,13 @@ console.log(formData);
                       <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"       onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} required/>
                   </div>
                  
-                  <Button  type="submit" >create an account</Button>
+                  <Button  type="submit"  >create an account</Button>
+
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400" onClick={handleloginclick}>
                       Already have an account? <a href="" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
                   </p>
               </form>
+              
           </div>
       </div>
   </div>
