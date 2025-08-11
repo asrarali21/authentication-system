365eab98f59b8f43fed3361036ca94d8e69c2779
asrarali21 <mohdasrar3825@gmail.com>
2025-08-11 21:12:14 +0530

    input field clear fix

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -42,16 +42,16 @@ console.log(response);
               <form class="space-y-4 md:space-y-6"  onSubmit={handleformsubmit}>
                     <div>
                       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">name</label>
-                      <input type="text" name="name" id="name" placeholder="enter you name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})}required/>
+                      <input type="text" name="name" id="name" placeholder="enter you name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={formData.name} onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})}required/>
                   </div>
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} required/>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" value={formData.email}  onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} required/>
                   </div>
                  
                   <div>
                       <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"       onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} required/>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"    value={formData.password}    onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} required/>
                   </div>
                  
                   <Button  type="submit"  >create an account</Button>
