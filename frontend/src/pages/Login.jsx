dff8bc61e4f78f65b8d94a4a164c7799a09a0fc9
asrarali21 <mohdasrar3825@gmail.com>
2025-08-12 11:03:41 +0530

    user register successfully frontend and in process login logic

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,55 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { handleError, handlesuccess } from '@/util'
 
 function Login() {
     const navigate = useNavigate()
     function handleregisterclick() {
         navigate("/register")
     }
+
+
+  const [logininfo, setLogininfo] = useState({
+  email: "",
+  password: ""
+});
+
+console.log(logininfo);
+
+
+  
+   async function handleformsubmit(e) {
+      e.preventDefault();
+      const { email  , password} =logininfo
+   if (!email || !password) {
+  return handleError("all fields are required")
+    }
+    try {
+     const response =  await  axios.post("http://localhost:8000/api/v1/users/loginUser" , logininfo ,{
+   withCredentials: true
+    })
+    console.log(response.data.message);
+    handlesuccess(response.data.message)
+     setTimeout(()=>{
+      setLogininfo({  email: "", password: "" });
+       navigate("/home")
+    }, 1500)
+ 
+    } catch (error) {
+    handleError(error)
+    console.log(error);
+    
+     }
+   }
+   
   return (
     <div>
 
-        <div class="flex h-[700px] w-full">
-    <div class="w-full flex flex-col items-center justify-center">
+        <div className="flex h-[700px] w-full">
+    <div className="w-full flex flex-col items-center justify-center">
 
-        <form class="md:w-96 w-80 flex flex-col items-center justify-center">
-            <h2 class="text-4xl text-gray-900 font-medium">Sign in</h2>
-            <p class="text-sm text-gray-500/90 mt-3">Welcome back! Please sign in to continue</p>
+        <form className="md:w-96 w-80 flex flex-col items-center justify-center" onSubmit={handleformsubmit}>
+            <h2 className="text-4xl text-gray-900 font-medium">Sign in</h2>
+            <p className="text-sm text-gray-500/90 mt-3">Welcome back! Please sign in to continue</p>
 
         
 
-            <div class="flex items-center gap-4 w-full my-5">
-                <div class="w-full h-px bg-gray-300/90"></div>
-                <p class="w-full text-nowrap text-sm text-gray-500/90">or sign in with email</p>
-                <div class="w-full h-px bg-gray-300/90"></div>
+            <div className="flex items-center gap-4 w-full my-5">
+                <div className="w-full h-px bg-gray-300/90"></div>
+                <p className="w-full text-nowrap text-sm text-gray-500/90">or sign in with email</p>
+                <div className="w-full h-px bg-gray-300/90"></div>
             </div>
 
-            <div class="flex items-center w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
+            <div className="flex items-center w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
                 <svg width="16" height="11" viewBox="0 0 16 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M0 .55.571 0H15.43l.57.55v9.9l-.571.55H.57L0 10.45zm1.143 1.138V9.9h13.714V1.69l-6.503 4.8h-.697zM13.749 1.1H2.25L8 5.356z" fill="#6B7280"/>
                 </svg>
-                <input type="email" placeholder="Email id" class="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full" required/>                 
+                <input type="email" placeholder="Email id" className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full" required/>                 
             </div>
 
-            <div class="flex items-center mt-6 w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
+            <div className="flex items-center mt-6 w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
                 <svg width="13" height="17" viewBox="0 0 13 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M13 8.5c0-.938-.729-1.7-1.625-1.7h-.812V4.25C10.563 1.907 8.74 0 6.5 0S2.438 1.907 2.438 4.25V6.8h-.813C.729 6.8 0 7.562 0 8.5v6.8c0 .938.729 1.7 1.625 1.7h9.75c.896 0 1.625-.762 1.625-1.7zM4.063 4.25c0-1.406 1.093-2.55 2.437-2.55s2.438 1.144 2.438 2.55V6.8H4.061z" fill="#6B7280"/>
                 </svg>
-                <input type="password" placeholder="Password" class="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full" required/>
+                <input type="password" placeholder="Password" className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full" required/>
             </div>
 
-            <div class="w-full flex items-center justify-between mt-8 text-gray-500/80">
-                <div class="flex items-center gap-2">
-                    <input class="h-5" type="checkbox" id="checkbox"/>
-                    <label class="text-sm" for="checkbox">Remember me</label>
+            <div className="w-full flex items-center justify-between mt-8 text-gray-500/80">
+                <div className="flex items-center gap-2">
+                    <input className="h-5" type="checkbox" id="checkbox"/>
+                    <label className="text-sm" for="checkbox">Remember me</label>
                 </div>
-                <a class="text-sm underline" href="#">Forgot password?</a>
+                <a className="text-sm underline" href="#">Forgot password?</a>
             </div>
 
-            <button type="submit" class="mt-8 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity">
+            <button type="submit" className="mt-8 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity">
                 Login
             </button>
-            <p class="text-gray-500/90 text-sm mt-4" onClick={handleregisterclick}>Don’t have an account? <a class="text-indigo-400 hover:underline" href="">Sign up</a></p>
+            <p className="text-gray-500/90 text-sm mt-4" onClick={handleregisterclick}>Don’t have an account? <a className="text-indigo-400 hover:underline" href="">Sign up</a></p>
         </form>
     </div>
 </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { handleError, handlesuccess } from '@/util';
 
 function Register() {
 
@@ -19,45 +20,61 @@ console.log(formData);
       navigate("/login")
    }
 
-  async function handleformsubmit(e) {
+   async function handleformsubmit(e) {
       e.preventDefault();
-    const response =  await  axios.post("http://localhost:8000/api/v1/users/registerUser" , formData ,{
-  withCredentials: true
-})
-console.log(response);
-    setFormData({ name: "", email: "", password: "" });
+      const {name , email  , password} =formData
+   if (!name || !email || !password) {
+  return handleError("all fields are required")
+    }
+    try {
+     const response =  await  axios.post("http://localhost:8000/api/v1/users/registerUser" , formData ,{
+   withCredentials: true
+    })
+    console.log(response.data.message);
+    handlesuccess(response.data.message)
+     setTimeout(()=>{
+      setFormData({ name: "", email: "", password: "" });
+       navigate("/login")
+    }, 1500)
+ 
+    } catch (error) {
+    handleError(error)
+    console.log(error);
+    
+     }
 
+   
    }
 
   return (
     <div> 
-      <section class="bg-gray-50 dark:bg-gray-900">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <section className="bg-gray-50 dark:bg-gray-900">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
     
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Create an account
               </h1>
-              <form class="space-y-4 md:space-y-6"  onSubmit={handleformsubmit}>
+              <form className="space-y-4 md:space-y-6"  onSubmit={handleformsubmit}>
                     <div>
-                      <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">name</label>
-                      <input type="text" name="name" id="name" placeholder="enter you name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={formData.name} onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})}required/>
+                      <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">name</label>
+                      <input type="text" name="name" id="name" placeholder="enter you name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={formData.name} onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})}/>
                   </div>
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" value={formData.email}  onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} required/>
+                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" value={formData.email}  onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} />
                   </div>
                  
                   <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"    value={formData.password}    onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} required/>
+                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"    value={formData.password}    onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} />
                   </div>
                  
                   <Button  type="submit"  >create an account</Button>
 
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400" onClick={handleloginclick}>
-                      Already have an account? <a href="" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400" onClick={handleloginclick}>
+                      Already have an account? <a href="" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
                   </p>
               </form>
               
